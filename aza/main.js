f2f307784daa46aac678253f361d68019169d582
elyas-code <73772366+elyas-code@users.noreply.github.com>
2023-09-24 09:21:22 +0300

    Update main.js

--- aza/main.js
+++ aza/main.js
@@ -100,9 +100,9 @@ function loadTrack(track_index) {
     function random_bg_color() {
     // Get a random number between 64 to 256
     // (for getting lighter colors)
-    let red = Math.floor(Math.random() * 100);
-    let green = Math.floor(Math.random() * 100);
-    let blue = Math.floor(Math.random() * 100);
+    let red = Math.floor(Math.random() * 100 + 10);
+    let green = Math.floor(Math.random() * 100 + 10);
+    let blue = Math.floor(Math.random() * 100 + 10);
     
     // Construct a color with the given values
     let bgColor = "rgb(" + red + ", " + green + ", " + blue + ")";
