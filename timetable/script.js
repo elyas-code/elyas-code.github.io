27fd4b2288e3965cca09005ba140ad5ebee2fb16
elyas-code <73772366+elyas-code@users.noreply.github.com>
2024-10-13 09:27:27 +0300

    push

--- timetable/script.js
+++ timetable/script.js
@@ -1,31 +1,25 @@
 // Sample timetable data with days of the week
 const timetable = {
-    0: [ // Sunday
+    1: [ // Sunday
         { subject: "DB201", teacher: "DR", room: "C2", start: "07:15", end: "08:45" },
         { subject: "OS201", teacher: "AA", room: "C2", start: "08:00", end: "9:30" },
         { subject: "Break", teacher: "N/A", room: "N/A", start: "09:30", end: "9:50" },
         { subject: "OS201", teacher: "AA", room: "C2", start: "09:50", end: "10:35" },
     ],
-    1: [ // Monday
+    2: [ // Monday
         { subject: "WEB102", teacher: "B8 AK", room: "WEB102", start: "08:45", end: "09:30" },
         { subject: "Math", teacher: "B6 YA", room: "Math", start: "09:50", end: "10:35" },
     ],
-    2: [ // Tuesday
+    3: [ // Tuesday
         { subject: "Phy", teacher: "FM", room: "Ph. Lab", start: "10:35", end: "11:20" },
         { subject: "Chem", teacher: "RY / SS", room: "CH.Lab", start: "11:20", end: "12:05" },
     ],
-    3: [ // Wednesday
+    4: [ // Wednesday
         { subject: "Islam", teacher: "A8 NS", room: "Islam", start: "12:05", end: "12:45" },
         { subject: "PE", teacher: "O", room: "PE", start: "13:05", end: "13:45" },
     ],
-    4: [ // Thursday
+    5: [ // Thursday
         { subject: "DB201", teacher: "C2 DR", room: "DB20", start: "07:15", end: "08:00" },
-    ],
-    5: [ // Friday
-        { subject: "OS20", teacher: "C2 AA", room: "OS20", start: "08:00", end: "08:45" },
-    ],
-    6: [ // Saturday
-        // No classes
     ]
 };
 
