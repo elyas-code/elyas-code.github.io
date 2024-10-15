8d8d3967b6fe8d70def793b666fed621658db103
Ali AlQassab <109420150+Alawi8553@users.noreply.github.com>
2024-10-15 10:42:18 +0300

    script.js
    added Thursday timing for AI + CCP

--- timetable/script.js
+++ timetable/script.js
@@ -43,14 +43,15 @@ const timetables = {
             { subject: "DB201", teacher: "DR", room: "C1", start: "13:05", end: "13:45" },
         ],
         4: [
-            { subject: "PHY201", teacher: "FM", room: "BB1", start: "07:15", end: "08:45" },
-            { subject: "PE", teacher: "O", room: "PE", start: "08:45", end: "09:30" },
-            { subject: "Break", teacher: "N/A", room: "N/A", start: "09:30", end: "09:50" },
-            { subject: "PE-S", teacher: "New/O", room: "PE", start: "09:50", end: "10:35" },
-            { subject: "ARC201", teacher: "MS/IA", room: "C1", start: "10:35", end: "12:05" },
-            { subject: "ENG201", teacher: "JP", room: "A2", start: "12:05", end: "12:45" },
-            { subject: "Break", teacher: "N/A", room: "N/A", start: "12:45", end: "13:05" },
-            { subject: "ENG201", teacher: "JP", room: "A2", start: "13:05", end: "13:45" },
+            { subject: "PHY201", teacher: "FM", room: "BB1", start: "07:15", end: "08:20" },
+            { subject: "PE", teacher: "O", room: "PE", start: "08:20", end: "09:00" },
+            { subject: "Break", teacher: "N/A", room: "N/A", start: "09:00", end: "09:15" },
+            { subject: "PE-S", teacher: "New/O", room: "PE", start: "09:15", end: "9:55" },
+            { subject: "ARC201", teacher: "MS/IA", room: "C1", start: "09:55", end: "11:15" },
+            { subject: "ENG201", teacher: "JP", room: "A2", start: "11:15", end: "11:55" },
+            { subject: "Break", teacher: "N/A", room: "N/A", start: "11:55", end: "12:10" },
+            { subject: "ENG201", teacher: "JP", room: "A2", start: "12:10", end: "12:50" },
+            { subject: "Club", teacher: "N/A", room: "N/A", start: "12:55", end: "13:45" },
         ]
     },
     1: { // 11.AI
@@ -98,14 +99,15 @@ const timetables = {
             
         ],
         4: [
-            { subject: "MAT201", teacher: "YA", room: "B6", start: "07:15", end: "08:45" },
-            { subject: "ML201", teacher: "SG", room: "C1", start: "08:45", end: "09:30" },
-            { subject: "Break", teacher: "N/A", room: "N/A", start: "09:30", end: "09:50" },
-            { subject: "ML201", teacher: "SG", room: "C1", start: "09:50", end: "10:35" },
-            { subject: "ENG201", teacher: "JP", room: "A2", start: "10:35", end: "12:05" },
-            { subject: "WEB201", teacher: "AK", room: "C2", start: "12:05", end: "12:45" },
-            { subject: "Break", teacher: "N/A", room: "N/A", start: "12:45", end: "13:05" },
-            { subject: "WEB201", teacher: "AK", room: "C2", start: "13:05", end: "13:45" },
+            { subject: "MAT201", teacher: "YA", room: "B6", start: "07:00", end: "08:20" },
+            { subject: "ML201", teacher: "SG", room: "C1", start: "08:20", end: "09:00" },
+            { subject: "Break", teacher: "N/A", room: "N/A", start: "09:00", end: "09:15" },
+            { subject: "ML201", teacher: "SG", room: "C1", start: "09:15", end: "09:55" },
+            { subject: "ENG201", teacher: "JP", room: "A2", start: "09:55", end: "11:15" },
+            { subject: "WEB201", teacher: "AK", room: "C2", start: "11:15", end: "11:55" },
+            { subject: "Break", teacher: "N/A", room: "N/A", start: "11:55", end: "12:10" },
+            { subject: "WEB201", teacher: "AK", room: "C2", start: "12:10", end: "12:50" },
+            { subject: "Club", teacher: "N/A", room: "N/A", start: "12:55", end: "13:45" },
         ]
     },
     2: { // 11.SEC
@@ -161,6 +163,7 @@ const timetables = {
             { subject: "WEB201", teacher: "AK", room: "C2", start: "12:05", end: "12:45" },
             { subject: "Break", teacher: "N/A", room: "N/A", start: "12:45", end: "13:05" },
             { subject: "WEB201", teacher: "AK", room: "C2", start: "13:05", end: "13:45" },
+            
         ]
     }
 };
